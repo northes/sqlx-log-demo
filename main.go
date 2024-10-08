8b5a4f3adaf3f5b31e0006d7417fadbd2194d736
northes <me@northes.io>
2024-10-08 09:59:25 +0800

    feature(main): update

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	customdbdemo "go-sql-log/customdb"
 	"go-sql-log/logger"
+	sqldbloggerdemo "go-sql-log/sqldb-logger"
 	sqlhooksdemo "go-sql-log/sqlhooks"
 )
 
@@ -11,10 +13,10 @@ func init() {
 
 func main() {
 	// 在sqlx的层面实现，通过实现 sqlx 的相关接口，来达到打印日志的目的，相对来说更复杂（层级更高）
-	//customdb.Run()
+	customdbdemo.Run()
 
 	// 在driver层面实现，需要传入logger
-	//sqldbloggerdemo.Run()
+	sqldbloggerdemo.Run()
 
 	// 在driver的层面实现（层级更低），实现Before和After即可，相对更加灵活
 	sqlhooksdemo.Run()
